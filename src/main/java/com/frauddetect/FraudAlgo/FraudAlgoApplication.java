215836024c2fe07dac8ca6846475728cf6b53d7a
felixojiambo <felixojiamboe@gmail.com>
2024-12-25 17:04:12 +0300

    Initial commit: Set up Spring Boot project with required dependencies

---
+++ src/main/java/com/frauddetect/FraudAlgo/FraudAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.frauddetect.FraudAlgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudAlgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frauddetect/FraudAlgo/FraudAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frauddetect.FraudAlgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
